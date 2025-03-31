65e9b93535380aa1339ab6240212f9131f16a3a5
Magica-Faux <karaec@proton.me>
2025-03-31 18:10:09 +0200

    Update orgx.el

--- emacs/rei-modules/orgx.el
+++ emacs/rei-modules/orgx.el
@@ -2,19 +2,30 @@
 ;;  REI Core Module
 ;;  Magica-Faux * 2025 
 
-(setq org-adapt-indentation t
-      org-hide-leading-stars t
-      org-hide-emphasis-markers t
-      org-pretty-entities t
-	  org-ellipsis "  ·")
+(use-package org
+  :config
+  (setq org-adapt-indentation t
+        org-hide-leading-stars t
+        org-hide-emphasis-markers t
+        org-pretty-entities t
+        org-ellipsis "  ·"
+        org-src-fontify-natively t
+        org-src-tab-acts-natively t
+        org-edit-src-content-indentation 0))
 
-(require 'org-superstar)
-(add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
+(use-package org-superstar
+  :ensure t
+  :after org
+  :hook (org-mode . org-superstar-mode))
 
-(require 'olivetti)
-(add-hook 'org-mode-hook 'olivetti-mode)
+(use-package olivetti
+  :ensure t
+  :hook (org-mode . olivetti-mode))
 
-(add-hook 'org-mode-hook 'visual-line-mode) ; Adaptive Line Fill
+(require 'org-indent)
+  (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+
+(add-hook 'org-mode-hook 'visual-line-mode)
 (add-hook 'org-mode-hook 'variable-pitch-mode)
 (add-hook 'org-mode-hook (lambda () (display-line-numbers-mode -1)))
 
@@ -22,16 +33,6 @@
   (lambda ()
     (variable-pitch-mode 1) 
     (set-face-attribute 'variable-pitch nil :font "Inter Display-24")
-  
     (set-face-attribute 'org-code nil :inherit 'fixed-pitch)
     (set-face-attribute 'org-block nil :inherit 'fixed-pitch)
     (set-face-attribute 'org-table nil :inherit 'fixed-pitch)))
-
-
-(require 'org-indent)
-(set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
-
-;; Set Source code blocks to behave natively 
-(setq org-src-fontify-natively t
-	  org-src-tab-acts-natively t
-      org-edit-src-content-indentation 0)
