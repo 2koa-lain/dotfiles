3bda3cb00c6147a6b3d3a999181f6b0be1f3522d
Magica-Faux <karaec@proton.me>
2025-03-31 18:08:38 +0200

    use-package

--- emacs/init.el
+++ emacs/init.el
@@ -9,50 +9,64 @@
           ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
-(add-hook 'after-init-hook 'global-company-mode)
-(yas-global-mode 1)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
+(require 'use-package)
+(setq use-package-always-ensure t)
 
-(add-hook 'c-mode-hook 'lsp)
+(use-package company
+  :config
+  (global-company-mode))
+
+(use-package yasnippet
+  :config
+  (yas-global-mode 1))
+
+(use-package lsp-mode
+  :init
+  (setq lsp-headerline-breadcrumb-enable nil
+        lsp-clients-clangd-args '("--header-insertion=never"))
+  :hook
+  (c-mode . lsp))
+
+(use-package flymake
+  :init
+  (setq flymake-start-on-flymake-mode nil
+        flymake-start-on-save-buffer nil))
+
 (setq make-backup-files nil)
-(setq auto-save-default nil) ; # type files 
+(setq auto-save-default nil)
 (setq create-lockfiles nil)
 
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c-mode)) ; LSP gets confused when handling c headers
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c-mode))
 
-;; In case the directory name changes 
-;; we don't have to change it for every single file
 (setq modu-dir (expand-file-name "rei-modules/" user-emacs-directory))
-
-
 (setq custom-file (locate-user-emacs-file "comvars.el"))
 (load custom-file 'noerror 'nomessage)
-(load (expand-file-name "orgx.el" modu-dir))    ; Org Mode Config 
-(load (expand-file-name "display.el" modu-dir)) ; Theme, GUI Handling  
-(load (expand-file-name "keys.el" modu-dir))    ; Keybindings
+(load (expand-file-name "orgx.el" modu-dir))
+(load (expand-file-name "display.el" modu-dir))
+(load (expand-file-name "keys.el" modu-dir))
 
 (setq-default indent-tabs-mode t)
 (add-hook 'makefile-mode-hook (lambda () (setq indent-tabs-mode t)))
-(add-hook 'emacs-lisp-mode (lambda () (setq indent-tabs-mode nil))) 
+(add-hook 'emacs-lisp-mode (lambda () (setq indent-tabs-mode nil)))
 
-(setq lsp-headerline-breadcrumb-enable nil) ; Shitty ClangD top bar gone
-(setq lsp-clients-clangd-args
-      '("--header-insertion=never"))          ; Automatic Header insertion sucks
-(setq flymake-start-on-flymake-mode nil)
-(setq flymake-start-on-save-buffer nil)
-
-(setq-default tab-width 4) 
+(setq-default tab-width 4)
 (setq history-length 24)
-(setq-default right-margin-width 8) ; Define new widths
+(setq-default right-margin-width 8)
 
+(set-window-buffer nil (current-buffer))
 
-(set-window-buffer nil (current-buffer)) 
+(use-package savehist
+  :config (savehist-mode 1))
 
+(use-package saveplace
+  :config (save-place-mode 1))
 
-(savehist-mode 1)  ; Remember Minibuffer prompts
-(save-place-mode 1)
-(recentf-mode 1)   ; Remember recent files
+(use-package recentf
+  :config (recentf-mode 1))
 
 (put 'erase-buffer 'disabled nil)
 (put 'upcase-region 'disabled nil)
-
