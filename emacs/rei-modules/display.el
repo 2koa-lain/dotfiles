3662433cc773f3dc1144e0ffa04630b05dabd2cf
Magica-Faux <karaec@proton.me>
2025-03-31 18:09:14 +0200

    Update display.el

--- emacs/rei-modules/display.el
+++ emacs/rei-modules/display.el
@@ -2,25 +2,26 @@
 ;;  REI Core Module
 ;;  Magica-Faux * 2025 
 
+(use-package gruvbox-theme
+  :ensure t
+  :config
+  (load-theme 'gruvbox-dark-hard t))
+
 (when (display-graphic-p)
   (setq default-frame-alist 
-        '((font . "Iosevka Term-24") ; GUI Font
+        '((font . "Unifont-24")
           (vertical-scroll-bars . nil)
-          (horizontal-scroll-bars . nil)))
-  (load-theme 'meadow-dark' t))
-
-;; Uncomment this if you're on OSX
-;(add-to-list 'default-frame-alist '(undecorated . t))
+          (horizontal-scroll-bars . nil))))
 
 (when (display-graphic-p)
-  (scroll-bar-mode -1)      ; Kill scrollbar
-  (set-fringe-mode 0)       ; Kill fringe
-  (window-divider-mode -1)) ; Kill window dividers
+  (scroll-bar-mode -1)
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (set-fringe-mode 0)
+  (window-divider-mode -1))
  
-(setq  inhibit-startup-message t ; Disable Splash Screen
-       visible-bell nil)
+(setq inhibit-startup-message t
+      visible-bell nil)
 
-(menu-bar-mode -1)
-(tool-bar-mode nil)
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
-(xterm-mouse-mode t) ; Enable mouse cursor support in the terminal
+(xterm-mouse-mode t)
