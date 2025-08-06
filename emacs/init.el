b97c66546389d70da8042dcf86f6e7013a52b527
Magica-Faux <karaec@proton.me>
2025-08-06 04:36:44 -0700

    Add files via upload

--- emacs/init.el
+++ emacs/init.el
@@ -1,9 +1,9 @@
-;;; INIT - Main Init File for the config
+;;; INIT - Main Init File for the config  -*- lexical-binding: t; -*-
 ;;  MAIN
 ;;  Magica-Faux * 2025 
 
 (require 'package)
-(setopt package-archives
+(setq package-archives
         '(("gnu" . "https://elpa.gnu.org/packages/")
           ("nongnu" . "https://elpa.nongnu.org/nongnu/")
           ("melpa" . "https://melpa.org/packages/")))
@@ -19,6 +19,30 @@
 (use-package company
   :config
   (global-company-mode))
+
+(use-package treemacs
+  :ensure t
+  :init
+  (with-eval-after-load 'winum
+	(define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (setq treemacs-indentation 2
+		treemacs-eldoc-display 'simple))
+
+
+(use-package hl-todo
+  :ensure t
+  :config
+  (setq hl-todo-keyword-faces
+		'(("TODO"    . "orange")
+		  ("WARNING" . "red")
+		  ("FIXME"   . "green")
+		  ("STUB"    . "seashell3")
+		  ("NOTE"    . "yellow")))
+  (global-hl-todo-mode 1))
+  
+  
+  
 
 (use-package yasnippet
   :config
@@ -36,6 +60,8 @@
   (setq flymake-start-on-flymake-mode nil
         flymake-start-on-save-buffer nil))
 
+(use-package magit)
+
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 (setq create-lockfiles nil)
@@ -49,7 +75,7 @@
 (load (expand-file-name "display.el" modu-dir))
 (load (expand-file-name "keys.el" modu-dir))
 
-(setq-default indent-tabs-mode t)
+(setq indent-tabs-mode t)
 (add-hook 'makefile-mode-hook (lambda () (setq indent-tabs-mode t)))
 (add-hook 'emacs-lisp-mode (lambda () (setq indent-tabs-mode nil)))
 
@@ -70,3 +96,27 @@
 
 (put 'erase-buffer 'disabled nil)
 (put 'upcase-region 'disabled nil)
+
+
+(defun ada-mode--find-alire (dir)
+  ;; Look up-tree, starting at dir, for alire.toml; return its full
+  ;; path name if found, nil if not.
+  (let ((alire (locate-dominating-file dir "alire.toml")))
+    (if alire
+      (cons 'transient alire)
+      nil)))
+ (defun ada-mode--find-gpr (dir)
+  ;; Look up-tree, starting at dir, for a .gpr file, returning its
+  ;; full path name if found, nil if not.
+  (let ((gpr (locate-dominating-file
+              dir
+              (lambda (dir) (directory-files dir nil "gpr"))
+              )))
+    (if gpr
+      (cons 'transient gpr)
+      nil)))
+ (use-package project
+  :config
+  (add-hook 'project-find-functions #'ada-mode--find-gpr)
+  (add-hook 'project-find-functions #'ada-mode--find-alire)
+  )
